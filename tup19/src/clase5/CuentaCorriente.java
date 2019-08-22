48872a45f5f3e641ef81d2b67f8fb6e63e3a8d08
felipeperezg <53921531+felipeperezg@users.noreply.github.com>
2019-08-22 18:11:32 -0300

    Se subió el ejercicio de la clase 5

--- tup19/src/clase5/CuentaCorriente.java
+++ tup19/src/clase5/CuentaCorriente.java
@@ -13,10 +13,6 @@ public class CuentaCorriente {
 	}
 	public void setSaldo(double saldo){
 		this.saldo+=saldo;
-		if (saldo==0) {
-			System.out.println("No se pudo completar la transferencia.");
-			return;
-		}
 		System.out.println("Se incrementó su saldo. Su nuevo saldo es de: "+this.saldo);
 	}
 	public double getSaldo() {
@@ -25,11 +21,22 @@ public class CuentaCorriente {
 	public long getNroCuenta() {
 		return this.nroCuenta;
 	}
+	public String getTitular() {
+		return this.titular;
+	}
 	public String getInfo() {
-		return ("Nombre de titular: "+this.titular+"\nSaldo: "+this.saldo+"\nnroCuenta: "+this.nroCuenta);
+		return ("Nombre de titular: "+this.titular+"\nSaldo: "+this.saldo+"\nNúmero de cuenta: "+this.nroCuenta);
+	}
+	public void transferirSaldo(double saldo){
+		this.saldo+=saldo;
+		if (saldo==0) {
+			System.out.println("No se pudo completar la transferencia.");
+			return;
+		}
+		System.out.println("La transferencia se realizó satisfactoriamente.");
 	}
 	public double extraerSaldo(double saldo) {
-		if (this.saldo-saldo>0) {
+		if (this.saldo-saldo>=0) {
 			this.saldo-=saldo;
 			System.out.println("Se retiró dinero de su saldo. Su nuevo saldo es de: "+this.saldo);
 		}
--- tup19/src/clase5/main.java
+++ tup19/src/clase5/main.java
@@ -21,10 +21,7 @@ public class main {
 		CuentaCorriente[] nuevaCuenta=new CuentaCorriente[100];
 		nuevaCuenta[0]= new CuentaCorriente(titular,saldo);
 		nuevaCuenta[1]= new CuentaCorriente("Pedro",3500);
-		for (int x=2;x<nuevaCuenta.length;x++) {
-			nuevaCuenta[x]= new CuentaCorriente ("sos",300);
-		}
-		System.out.println("El número de cuenta de Pedro es: "+nuevaCuenta[1].getNroCuenta());
+		//Creamos una cuenta default para poder realizar trasnferencias
 		do {
 			System.out.println("¿Que operación desea realizar? 1=Agregar dinero 2=Extraer dinero 3=Obtener saldo 4=Obtener información completa 5=Transferir un dinero de una cuenta a otra.");
 			opcion=sc.nextInt();
@@ -43,26 +40,32 @@ public class main {
 				System.out.println("Su saldo es de: "+nuevaCuenta[0].getSaldo());
 				break;
 			case 4:
-				System.out.println(nuevaCuenta [0].getInfo());	
+				System.out.println(nuevaCuenta[0].getInfo());	
 				break;
 			case 5:
-				System.out.println("Ingrese la cuenta destino: ");
+				System.out.println("Cuentas disponibles: "+nuevaCuenta[1].getTitular()+" "+nuevaCuenta[1].getNroCuenta());
+				System.out.println("Ingrese el número de cuenta destino: ");
 				nroCuenta=sc.nextLong();
 				for (int x=0;x<nuevaCuenta.length;x++) {
 					if (nuevaCuenta[x].getNroCuenta() == nroCuenta) {
 						System.out.println("Ingrese el monto a transferir");
 						operacion=sc.nextDouble();
-						nuevaCuenta[x].setSaldo(nuevaCuenta[0].extraerSaldo(operacion));
+						nuevaCuenta[x].transferirSaldo(nuevaCuenta[0].extraerSaldo(operacion));
 						break;
 					}
 					z=x;
 				}
 				if (z==nuevaCuenta.length-1) {
-					System.out.println("Ese número de cuenta no esiste");
+					System.out.println("Ese número de cuenta no existe");
 				}
 			}
+			do {
 			System.out.println("Desea continuar realizando operaciones? S/N ");
 			continuar=sc.next().charAt(0);
+			if (continuar=='N' || continuar=='n') {
+				break;
+			}
+			}while (continuar!='S' && continuar!='s');
 		}while (continuar!='N' && continuar!='n');
 		System.out.println("Fin del programa.");
 		
